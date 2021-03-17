4fdb0bef8c686e500f94bbffeab6396af2d40f56
franctech <32376285+rosefrancis-tech@users.noreply.github.com>
2021-03-17 00:18:26 -0400

    save edited post on save button click

---
+++ public/javascript/edit-post.js
@@ -0,0 +1,28 @@
+async function editFormHandler(event) {
+    event.preventDefault();
+  
+    const blog_title = document.querySelector('input[name="post-title"]').value.trim();
+    const blog_body = document.querySelector('input[name="post-body"]').value.trim();
+    const id = window.location.toString().split('/')[
+      window.location.toString().split('/').length - 1
+    ];
+    const response = await fetch(`/api/posts/${id}`, {
+      method: 'PUT',
+      body: JSON.stringify({
+        blog_title,
+        blog_body
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+  
+    if (response.ok) {
+      document.location.replace('/dashboard/');
+    } else {
+      alert(response.statusText);
+    }
+  }
+  
+  document.querySelector('.edit-post-form').addEventListener('submit', editFormHandler);
+  
